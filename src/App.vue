aeba2095d0469898670e117f1bf66b98e5d149fa
Michelle Horsey <michelle@thedevhouse.uk>
2022-06-08 22:05:11 +0100

    styling

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,8 @@
 <template>
   <router-view />
 </template>
+<style>
+body{
+    background-color: #000033;
+}
+</style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -13,3 +13,23 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.card{
+    width: 10vw;
+    height: 30vh;
+    margin: 10px;
+    display: inline-block;
+
+    img{
+        width: 10vw;
+        height: 25vh;
+    }
+
+    p{
+        color: #6262be;
+        font-size: 0.7em;
+        font-family: sans-serif;
+    }
+}
+</style>
